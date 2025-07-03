100e32873726d01e34b3f476bb33c1d4bbf4f301
SeaPiseth <darkworriorkh@gmail.com>
2025-07-03 15:51:11 +0700

    Work-Flow

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, GitHub Actions!');
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
-
+// Export the app for testing
 module.exports = app;
+
+// Start the server only if this file is run directly
+if (require.main === module) {
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
+}
--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 const request = require('supertest');
 const app = require('../app');
+require('should');
 
 describe('GET /', () => {
   it('should return Hello, GitHub Actions!', async () => {
     const res = await request(app).get('/');
     res.text.should.equal('Hello, GitHub Actions!');
+    res.statusCode.should.equal(200);
   });
 });
